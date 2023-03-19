fabbf0c4985b7b514221e645e5f724a4d88f42c4
ihabsouey <ihab.souey@gmail.com>
2023-03-19 10:35:01 +0100

    impor

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Button({ text, onClick }) {
-  return <button onClick={onClick}>Click me {text}</button>;
+export default function Button({ onClick }) {
+  return <button  className="btn" onClick={onClick}>Send ! </button>;
 }
